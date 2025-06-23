1e26d1a8e586b5db109567c41f7b6b5eaf06671f
NallaniRevathi <151653253+NallaniRevathi@users.noreply.github.com>
2025-06-23 15:18:28 +0530

    Add files via upload

---
+++ week1/day1/TestLeaf.java
@@ -0,0 +1,11 @@
+package week1.day1;
+
+public class TestLeaf {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Welcome To TestLeaf");
+		
+	}
+
+}
---
+++ week1/day2/AccessModifier.java
@@ -0,0 +1,14 @@
+package week1.day1;
+
+public class AccessModifier {
+	
+	public static void main(String[] args) {
+		Browser br=new Browser();
+		br.launchBrowser("Edge");
+		br.loadUrl();
+		
+		
+
+	}
+
+}
