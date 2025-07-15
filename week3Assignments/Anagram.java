54f002790732ebb8bbc7986fa856d91d22c3470c
NallaniRevathi <151653253+NallaniRevathi@users.noreply.github.com>
2025-07-15 20:21:46 +0530

    Update Anagram.java

--- week3Assignments/Anagram.java
+++ week3Assignments/Anagram.java
@@ -7,17 +7,17 @@ public class Anagram {
 	public static void main(String[] args) {
 		String text1 = "stoped";  
 	 	String text2 = "posted";
-	 	
+//to check anagram or not	 	
 	 	if (text1.length()!=text2.length()) {
 	 		System.out.println("Lengths mismatch, therefore the strings are not an Anagram");
 	 	}
 	 	else {
 	 		char[] charArray1 = text1.toCharArray();
 	 		char[] charArray2 = text2.toCharArray();
-	 		
+//to sort array	 		
 	 		Arrays.sort(charArray1);
 	 		Arrays.sort(charArray2);
-	 		
+//checking for equal or not	 		
 	 		if (Arrays.equals(charArray1, charArray2)) {
 	 			System.out.println("The given strings are Anagram");
 	 		}
