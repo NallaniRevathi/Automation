29163479d92ab459a1a363a342c9eb2d2d3b53df
NallaniRevathi <151653253+NallaniRevathi@users.noreply.github.com>
2025-07-15 20:23:30 +0530

    Update CheckboxButton.java

--- week3Assignments/CheckboxButton.java
+++ week3Assignments/CheckboxButton.java
@@ -1,5 +1,5 @@
 package week3Assignments;
-
+//inheriting from subclass1
 public class CheckboxButton extends Buttonsubclass {
 	public void clickCheckButton() {
         System.out.println("Checkbox button clicked----->Subclass2");
