513cfa64ee23ca86ac125d75d6b2a37b4f6ee8bc
NallaniRevathi <151653253+NallaniRevathi@users.noreply.github.com>
2025-07-15 20:27:50 +0530

    Update Intersection.java

--- week3Assignments/Intersection.java
+++ week3Assignments/Intersection.java
@@ -6,6 +6,7 @@ public class Intersection {
 		int a[] = { 3, 2, 11, 4, 6, 7 };
 		int b[] = { 1, 2, 8, 4, 9, 7 };
 		System.out.print("The Intersection of two Array is:");
+		//condition for common values
 		for (int i = 0; i < a.length; i++) {
 			for (int j = 0 + 1; j < b.length; j++) {
 				if (a[i] == b[j]) {
