618aa8c0aba600a3322e9b9633c8ab9a5b915a22
NallaniRevathi <151653253+NallaniRevathi@users.noreply.github.com>
2025-07-15 20:26:50 +0530

    Update FindMissingValue.java

--- week3Assignments/FindMissingValue.java
+++ week3Assignments/FindMissingValue.java
@@ -6,9 +6,12 @@ public class FindMissingValue {
 
 	public static void main(String[] args) {
 		int a[] = { 1, 4, 3, 2, 8, 6, 7 };
+		//sorting array
 		Arrays.sort(a);
 		int i = a[0];
+		//iteration
 		while (i < a.length) {
+			//condition for missing value
 			if (a[i] - a[i - 1] == 1) {
 			} else {
 				System.out.println("Missing number is " + (a[i - 1] + 1));
