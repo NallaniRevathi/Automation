cec68ad0537cbcc42f87c4b826fd650eb9e510ec
NallaniRevathi <151653253+NallaniRevathi@users.noreply.github.com>
2025-08-05 23:06:20 +0530

    Add files via upload

---
+++ marathon3/CreateAccount.java
@@ -0,0 +1,41 @@
+package marathon3;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+public class CreateAccount extends BaseClass{
+	
+	//Created a variable String to pass the workbook name
+	@BeforeTest
+	public void setValues() {
+		ExcelWorkBook = "CreateAccount"; 
+	}
+	
+	//Data provider fetched data will be passed to the test
+	@Test(dataProvider = "fetchData")
+    public void runCreateAccount(String accName) throws InterruptedException {
+		
+		//Click 'New' to create a new account
+        driver.findElement(By.xpath("//a[@title='New']")).click();
+        Thread.sleep(3000);
+        //Passed the required details
+        driver.findElement(By.xpath("//label[text()='Account Name']/following::input")).sendKeys(accName);
+        WebElement ownershipDD = driver.findElement(By.xpath("//button[@aria-label='Ownership']"));
+        driver.executeScript("arguments[0].click()",ownershipDD);
+        Thread.sleep(3000);
+        driver.findElement(By.xpath("//span[@title='Public']")).click();
+        Thread.sleep(3000);
+        //Saved 
+        driver.findElement(By.xpath("//button[@name='SaveEdit']")).click();
+        
+        //ToastMessage has been printed to confirm
+        WebElement toastMessage = driver.findElement(By.xpath("//span[@class='toastMessage slds-text-heading--small forceActionsText']"));
+        String text = toastMessage.getText();
+        System.out.println(text);
+        Thread.sleep(3000);
+        //Assertion to verify if the name passed is present in the ToastMessage
+        Assert.assertTrue(text.contains(accName),"Verify the Account name");
+    }
+
+}
---
+++ marathon3/EditAccount.java
@@ -0,0 +1,86 @@
+package marathon3;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+public class EditAccount extends BaseClass{
+	
+	//Created a variable String to pass the workbook name
+	@BeforeTest
+	public void setValues() {
+		ExcelWorkBook = "EditAccount"; 
+	}
+	
+	//Data provider fetched data will be passed to the test
+	@Test(dataProvider = "fetchData")
+	
+	//Creating a variable and passing as argument to pass data from ExcelWorkBook
+	public void runEditAccount(String accName, String BillingStreet, String ShippingStreet) throws InterruptedException {
+    
+	//Passed the required details retrieved from the ExcelWorkBook
+    driver.findElement(By.xpath("//input[@placeholder='Search this list...']")).sendKeys(accName+ Keys.ENTER);
+    
+    Thread.sleep(3000);
+    
+    //Click the dropdown and click Edit button
+    driver.findElement(By.xpath("//table/tbody/tr[1]/td[6]")).click();
+    driver.findElement(By.xpath("//a[@title='Edit']")).click();
+    Thread.sleep(3000);
+    //Filling all the required details and passing the data from Excel WorkBook under BillingStreet and ShippingStreet
+    WebElement typeDD = driver.findElement(By.xpath("//button[@aria-label='Type']"));
+    driver.executeScript("arguments[0].click()",typeDD);
+    driver.findElement(By.xpath("//lightning-base-combobox-item[@data-value='Technology Partner']")).click();
+    Thread.sleep(3000);
+    WebElement industryDD = driver.findElement(By.xpath("//button[@aria-label='Industry']"));
+    driver.executeScript("arguments[0].click()",industryDD);
+    driver.findElement(By.xpath("//lightning-base-combobox-item[@data-value='Healthcare']")).click();
+    Thread.sleep(3000);
+    WebElement biilingStreet = driver.findElement(By.xpath("//label[text()='Billing Street']/following::textarea"));
+    biilingStreet.clear();
+    biilingStreet.sendKeys(BillingStreet);
+    Thread.sleep(3000);
+    WebElement shippingStreet = driver.findElement(By.xpath("(//textarea[@class='slds-textarea'])[2]"));
+    shippingStreet.clear();
+    shippingStreet.sendKeys(ShippingStreet);
+    Thread.sleep(3000);
+    WebElement priorityDD = driver.findElement(By.xpath("//button[@aria-label='Customer Priority']"));
+    driver.executeScript("arguments[0].click()",priorityDD);
+    Thread.sleep(3000);
+    driver.findElement(By.xpath("//lightning-base-combobox-item[@data-value='Low']")).click();
+    driver.findElement(By.xpath("//button[@aria-label='SLA']")).click();
+    driver.findElement(By.xpath("//lightning-base-combobox-item[@data-value='Silver']")).click();
+    WebElement activeDD = driver.findElement(By.xpath("//button[@aria-label='Active']"));
+    Thread.sleep(3000);
+    driver.executeScript("arguments[0].click()",activeDD);
+    driver.findElement(By.xpath("//lightning-base-combobox-item[@data-value='No']")).click();
+    
+    //creating a random number integer to pass as a phone number and printing it to verify
+    int randomNum1 = (int)(Math.random()*999999);
+    int randomNum2 = (int)(Math.random()*999999);
+    String phno = ""+randomNum1+randomNum2;
+    phno = phno.substring(0,10);
+    System.out.println("The phone number is: " + phno);
+    WebElement phnoField = driver.findElement(By.xpath("//input[@name='Phone']"));
+    phnoField.clear();
+    phnoField.sendKeys(phno);
+    Thread.sleep(3000);
+    //selecing Opportunity and click Save button
+    WebElement upsellOpportunityDD = driver.findElement(By.xpath("//button[@aria-label='Upsell Opportunity']"));
+    driver.executeScript("arguments[0].click()",upsellOpportunityDD);
+    driver.findElement(By.xpath("//lightning-base-combobox-item[@data-value='No']")).click();
+    driver.findElement(By.xpath("//button[@name='SaveEdit']")).click();
+    
+    Thread.sleep(3000);
+    
+    //Retrieving the phone number added while editing and printing it
+    WebElement phnoStr = driver.findElement(By.xpath("//table/tbody/tr[1]/td[4]"));
+    String text = phnoStr.getText();
+    System.out.println("The updated phone number is " + text); 
+    
+    //Using assertion to verify the phone number 
+    Assert.assertTrue(text.equals(phno), "verify the mobile Number"); 
+}
+
+}
